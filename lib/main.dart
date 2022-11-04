eda111a3d1b45d22de361ab1c8988186045b067d
Dzhanaliev Aktan <88595595+MorgDzh@users.noreply.github.com>
2022-11-04 09:34:58 +0600

    Added magic_ball_app :rocket:

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:neobisui/apps/magic_ball_app.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,12 +7,32 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Title"),
-          centerTitle: true,
+      home: MyHomePage(),
+      routes: {
+        '/magic_ball': (context) => BallPage(),
+      },
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Neobis UI'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/magic_ball');
+                },
+                child: Text('Magic Ball')),
+          ],
         ),
-        body: Container(child: Text("Text")),
       ),
     );
   }
