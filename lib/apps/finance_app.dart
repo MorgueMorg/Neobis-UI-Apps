0b6b23169078810d6b27a95ed761545da1d83566
Dzhanaliev Aktan <88595595+MorgDzh@users.noreply.github.com>
2022-11-06 03:21:26 +0600

    Added needed lists for finance :moneybag:

--- lib/apps/finance_app.dart
+++ lib/apps/finance_app.dart
@@ -6,6 +6,61 @@ class FinanceApp extends StatefulWidget {
 }
 
 class _MyHomePageState extends State <FinanceApp> {
+  final List<String> _listTitles= [
+    'Дом',
+    'Покупки',
+    'Транспорт',
+    'Здоровье',
+    'Фитнес',
+    'Счета',
+    'Ресторан',
+  ];
+
+  final List<String> _listDesc = [
+    'Продукты',
+    'Одежда',
+    'Такси',
+    'Лечение',
+    'Тренировки',
+    'Коммунальные',
+    'Ужин',
+  ];
+
+  final List<double> _listSpent= [
+    321,
+    574,
+    124,
+    756,
+    324,
+    726,
+    325,
+  ];
+
+  final List<Widget> _listImages = [
+    IconButton(
+        onPressed: () {},
+        icon: Image.asset('assets/financeImages/home.png', width: 50, height: 50)),
+    IconButton(
+        onPressed: () {},
+        icon: Image.asset('assets/financeImages/tag.png', width: 50, height: 50)),
+    IconButton(
+        onPressed: () {},
+        icon: Image.asset('assets/financeImages/happy.png', width: 50, height: 50)),
+    IconButton(
+        onPressed: () {},
+        icon: Image.asset('assets/financeImages/heart.png', width: 50, height: 50)),
+    IconButton(
+        onPressed: () {},
+        icon: Image.asset('assets/financeImages/cookie.png', width: 50, height: 50)),
+    IconButton(
+        onPressed: () {},
+        icon: Image.asset('assets/financeImages/mail.png', width: 50, height: 50)),
+    IconButton(
+        onPressed: () {},
+        // Я не знаю почему опять сердце, так в фигме
+        icon: Image.asset('assets/financeImages/heart.png', width: 50, height: 50)),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -13,7 +68,7 @@ class _MyHomePageState extends State <FinanceApp> {
         appBar: AppBar(
           title: Text("Finance"),
         ),
-        body: Container(child: Text("Finance")),
+        body: null,
       ),
     );
   }
