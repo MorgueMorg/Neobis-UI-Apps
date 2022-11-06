397b8e1ee1bce9fb0fcd798774293173fecc7b62
Dzhanaliev Aktan <88595595+MorgDzh@users.noreply.github.com>
2022-11-06 21:11:26 +0600

    Fixed chip in finance :wrench:

--- lib/apps/finance_app.dart
+++ lib/apps/finance_app.dart
@@ -5,8 +5,8 @@ class FinanceApp extends StatefulWidget {
   State<FinanceApp> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State <FinanceApp> {
-  final List<String> _listTitles= [
+class _MyHomePageState extends State<FinanceApp> {
+  final List<String> _listTitles = [
     'Дом',
     'Покупки',
     'Транспорт',
@@ -26,7 +26,7 @@ class _MyHomePageState extends State <FinanceApp> {
     'Ужин',
   ];
 
-  final List<double> _listSpent= [
+  final List<int> _listSpent = [
     321,
     574,
     124,
@@ -36,39 +36,152 @@ class _MyHomePageState extends State <FinanceApp> {
     325,
   ];
 
+  void _changeIndex(int index) {
+    setState(() {
+      // Пустой стейт для листвью билдера
+    });
+  }
+
   final List<Widget> _listImages = [
     IconButton(
         onPressed: () {},
-        icon: Image.asset('assets/financeImages/home.png', width: 50, height: 50)),
+        icon: Image.asset('assets/financeImages/home.png',
+            width: 50, height: 50)),
     IconButton(
         onPressed: () {},
-        icon: Image.asset('assets/financeImages/tag.png', width: 50, height: 50)),
+        icon:
+            Image.asset('assets/financeImages/tag.png', width: 50, height: 50)),
     IconButton(
         onPressed: () {},
-        icon: Image.asset('assets/financeImages/happy.png', width: 50, height: 50)),
+        icon: Image.asset('assets/financeImages/happy.png',
+            width: 50, height: 50)),
     IconButton(
         onPressed: () {},
-        icon: Image.asset('assets/financeImages/heart.png', width: 50, height: 50)),
+        icon: Image.asset('assets/financeImages/heart.png',
+            width: 50, height: 50)),
     IconButton(
         onPressed: () {},
-        icon: Image.asset('assets/financeImages/cookie.png', width: 50, height: 50)),
+        icon: Image.asset('assets/financeImages/cookie.png',
+            width: 50, height: 50)),
     IconButton(
         onPressed: () {},
-        icon: Image.asset('assets/financeImages/mail.png', width: 50, height: 50)),
+        icon: Image.asset('assets/financeImages/mail.png',
+            width: 100, height: 100)),
     IconButton(
         onPressed: () {},
         // Я не знаю почему опять сердце, так в фигме
-        icon: Image.asset('assets/financeImages/heart.png', width: 50, height: 50)),
+        icon: Image.asset('assets/financeImages/heart.png',
+            width: 200, height: 200)),
+  ];
+
+  final List<Color> chipColors = [
+    Colors.yellow.shade700,
+    Colors.purple.shade900,
+    Colors.lightGreen.shade500,
+    Colors.purple.shade600,
+    Colors.blue.shade800,
+    Colors.lightBlue.shade300,
+    Colors.purple.shade600,
   ];
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.white,
         appBar: AppBar(
-          title: Text("Finance"),
+          title: Text('Balance App UI'),
+          centerTitle: true,
         ),
-        body: null,
+        body: Center(
+          child: Column(
+            // Main column, everything in here
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Padding(
+                padding: EdgeInsets.only(top: 15),
+                child: Text(
+                  'Баланс',
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.all(20),
+                child: Text(
+                  '\$1200.89',
+                  style: TextStyle(
+                    fontSize: 32,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  IconButton(
+                      onPressed: () {},
+                      icon: const Icon(Icons.chevron_left_rounded)),
+                  const Text(
+                    'Апрель 2020',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                  IconButton(
+                      onPressed: () {},
+                      icon: const Icon(Icons.chevron_right_rounded)),
+                ],
+              ),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: _listTitles.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return ListTile(
+                      leading: Chip(
+                        label: _listImages[index],
+                        backgroundColor: chipColors[index],
+                        padding: EdgeInsets.all(0),
+                        labelPadding: EdgeInsets.all(0),
+                      ),
+                      title: Text(
+                        _listTitles[index],
+                        style: const TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      trailing: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Text(
+                            '\$${_listSpent[index]}',
+                            style: const TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                          Text(
+                            _listDesc[index],
+                            style: const TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.w400,
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
