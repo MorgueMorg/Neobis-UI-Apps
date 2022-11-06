1e138a04a77f81a4b7f9531ef99b21324ea3e8ee
Dzhanaliev Aktan <88595595+MorgDzh@users.noreply.github.com>
2022-11-07 03:54:00 +0600

    Finished 90% of travel_app :confetti_ball:

--- lib/apps/travel_app.dart
+++ lib/apps/travel_app.dart
@@ -10,15 +10,74 @@ class _MyHomePageState extends State<TravelApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-            title: Text("Travel"),
-            centerTitle: true,
+        appBar: AppBar(
+          toolbarHeight: 200,
+          backgroundColor: Colors.white,
+          elevation: 0,
+          title: Column(
+            children: [
+              Text(
+                'Путешествуйте',
+                style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.black),
+              ),
+              Text(
+                'Почувствуйте прилив энергии',
+                style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.black),
+              ),
+              SizedBox(height: 15,),
+              Text(
+                'Мы поможем вам исследовать, сравнить и',
+                style: TextStyle(fontSize: 14, color: Colors.black),
+              ),
+              Text(
+                'забронировать впечатления - все в одном',
+                style: TextStyle(fontSize: 14, color: Colors.black),
+              ),
+              Text(
+                'месте.',
+                style: TextStyle(fontSize: 14, color: Colors.black),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Expanded(
+                flex: 0,
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Куда хотите поехать?',
+                    hintStyle: TextStyle(color: Colors.blue),
+                    // suffixIconColor: Colors.blue,
+                    suffixIcon: Icon(Icons.search, color: Colors.blue,),
+                  ),
+                ),
+              ),
+            ],
           ),
-          body: Center(
-            child: Image(
-              image: AssetImage('assets/travelImages/abudhabi.png'),
-            ),
-          )),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: ListView(
+            children: [
+              Image(
+                image: AssetImage('assets/travelImages/abudhabi.png'),
+              ),
+              Image(
+                image: AssetImage('assets/travelImages/sanantonio.png'),
+              ),
+              Image(
+                image: AssetImage('assets/travelImages/abudhabi.png'),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
