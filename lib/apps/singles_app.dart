d2fb165769ebc212781686e6cdad2444d8eb4d87
Dzhanaliev Aktan <88595595+MorgDzh@users.noreply.github.com>
2022-11-07 22:14:19 +0600

    Added correct appbar :zap:

--- lib/apps/singles_app.dart
+++ lib/apps/singles_app.dart
@@ -1,16 +1,36 @@
 import 'package:flutter/material.dart';
 
+/* Во фрейме в фигме четко указано что это 11 айфон, поэтому верстал под его эмулятор */ 
 class SinglesApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Singles'),
+        automaticallyImplyLeading: false,
+        backgroundColor: Colors.white,
+        leading: Icon(
+          Icons.menu,
+          color: Colors.black,
+        ),
+        actions: [
+          Padding(
+            padding: EdgeInsets.only(right: 20),
+            child: Icon(
+              Icons.search,
+              color: Colors.black,
+            ),
+          )
+        ],
+        title: Text(
+          'Singles',
+          style: TextStyle(
+              color: Colors.black, fontSize: 22, fontWeight: FontWeight.w400),
+        ),
         centerTitle: true,
         elevation: 0,
       ),
       body: null,
-      backgroundColor: Colors.black,
+      backgroundColor: Colors.white,
     );
   }
 }
