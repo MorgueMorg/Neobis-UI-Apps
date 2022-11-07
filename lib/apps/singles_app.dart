f96c75fc8e3740a5d3a4124283f77bb85d6b7584
Dzhanaliev Aktan <88595595+MorgDzh@users.noreply.github.com>
2022-11-07 22:27:57 +0600

    Added images for pubspec and start body :sparkles:

--- lib/apps/singles_app.dart
+++ lib/apps/singles_app.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 
-/* Во фрейме в фигме четко указано что это 11 айфон, поэтому верстал под его эмулятор */ 
+/* Во фрейме в фигме четко указано что это 11 айфон, поэтому верстал под его эмулятор */
 class SinglesApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         automaticallyImplyLeading: false,
         backgroundColor: Colors.white,
+        centerTitle: true,
+        elevation: 0,
         leading: Icon(
           Icons.menu,
           color: Colors.black,
@@ -26,11 +29,25 @@ class SinglesApp extends StatelessWidget {
           style: TextStyle(
               color: Colors.black, fontSize: 22, fontWeight: FontWeight.w400),
         ),
-        centerTitle: true,
-        elevation: 0,
       ),
-      body: null,
-      backgroundColor: Colors.white,
+      body: Column(
+        children: [
+          Divider(),
+          Row(
+            children: [
+              Column(
+                children: [
+                  Text("SOS"),
+                  Text("Mini meltdown? Get some"),
+                  Text("Headspace in a hurry"),
+                ],
+              ),
+              Image(image: AssetImage('assets/singlesImages/Book.png')),
+              Icon(Icons.arrow_forward_ios_rounded)
+            ],
+          )
+        ],
+      ),
     );
   }
 }
